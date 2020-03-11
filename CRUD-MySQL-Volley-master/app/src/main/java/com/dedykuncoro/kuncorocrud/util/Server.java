0c153b409a46290632cd4ed4ccd246a938504e41
yandimul <34792016+yandimul@users.noreply.github.com>
2020-03-11 16:07:02 +0700

    CRUD-Volley-MySQL
    11 Maret 2020

---
+++ CRUD-MySQL-Volley-master/app/src/main/java/com/dedykuncoro/kuncorocrud/util/Server.java
@@ -0,0 +1,11 @@
+package com.dedykuncoro.kuncorocrud.util;
+
+/**
+ * Created by Kuncoro on 26/03/2016.
+ */
+public class Server {
+    /* 10.0.2.2 adalah IP Address localhost EMULATOR ANDROID STUDIO,
+    Ganti IP Address tersebut dengan IP Laptop Apabila di RUN di HP. HP dan Laptop harus 1 jaringan */
+    public static final String URL = "http://10.0.2.2/android/kuncoro_crud/";
+}
+
